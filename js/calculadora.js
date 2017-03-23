e1b43795873622f035d1174bffb15ecddfe7710e
marianajane <mariana.musm@gmail.com>
2017-03-23 15:03:19 -0600

    Primer commit del ejercicio, es proceso

---
+++ js/calculadora.js
@@ -0,0 +1,31 @@
+function suma() {
+	var listaFrutas= document.getElementById("sumando").value;
+    console.log (listaFrutas);
+}
+
+/*function numeros(){
+  
+}
+
+function sumar(numeros) {
+  
+  var suma = 
+  document.getElementById("sumando").value;
+  document.getElementById("espacio").innerHTML = "suma";
+}
+  return suma;
+
+
+}
+
+function restar(numeros) {
+  return ;
+}
+
+function multiplicar(numeros) {
+	return ;
+}
+
+function dividir(numeros) {
+	return ;
+}
